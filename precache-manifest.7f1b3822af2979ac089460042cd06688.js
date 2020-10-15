de1cda85e6d36286b44a2597b758e208043a584c
garima <garimajain096@gmail.com>
2020-10-15 10:17:51 +0530

    Updates

---
+++ precache-manifest.7f1b3822af2979ac089460042cd06688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0978177e722ff69508c85c9c204aef",
+    "url": "/chatapp/index.html"
+  },
+  {
+    "revision": "1a922cd7ac688e4d53ad",
+    "url": "/chatapp/static/css/main.8ac2bfff.chunk.css"
+  },
+  {
+    "revision": "90db5fe5db5a79451890",
+    "url": "/chatapp/static/js/2.2534a35b.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/chatapp/static/js/2.2534a35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a922cd7ac688e4d53ad",
+    "url": "/chatapp/static/js/main.95f81f30.chunk.js"
+  },
+  {
+    "revision": "29ec56799c069a8979bc",
+    "url": "/chatapp/static/js/runtime-main.cb55e0f3.js"
+  }
+]);
